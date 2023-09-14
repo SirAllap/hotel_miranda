561defc8d1b02a1180849dec9ed1d249c6eea4aa
David Pallarés <serallapdivad@gmail.com>
2023-09-14 14:36:05 +0100

    - Working on mobile first. - Create the menu for mobile - Create dropdown for the mobile menu - Added JS functionality to show the menu when user click on burger icon - Added logo & favicon

---
+++ index.js
@@ -0,0 +1,16 @@
+const icon_burger = document.querySelector('.header__mobile-burger-icon')
+const icon_close = document.querySelector('.header__mobile-close-icon')
+const drop_down = document.querySelector('.mobile__menu-list')
+
+document.addEventListener('click', (event) => {
+    if (event.target.matches('.header__mobile-burger-icon')) {
+        icon_burger.classList.add('hide')
+        icon_close.classList.add('show')
+        drop_down.classList.add('show')
+    }
+    if (event.target.matches('.header__mobile-close-icon')) {
+        icon_close.classList.remove('show')
+        icon_burger.classList.remove('hide')
+        drop_down.classList.remove('show')
+    }
+})
