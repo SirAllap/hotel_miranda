5c2329f599e72cef0a4d663722ce3c03116cfb80
David Pallarés <serallapdivad@gmail.com>
2023-09-18 14:23:11 +0100

    - Home facilities complete with swiper

--- index.js
+++ index.js
@@ -21,10 +21,12 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-
     // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: ".swiper-pagination",
     },
 })
